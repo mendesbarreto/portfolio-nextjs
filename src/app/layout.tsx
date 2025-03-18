f858b4e009ec2b534d965f0aec6d7881067d42ae
Douglas Mendes <mendes-barreto@live.com>
2025-03-17 22:58:58 -0400

    chore: Add overlay box

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Fira_Code } from "next/font/google";
 import "./globals.css";
+import "./fontAwesomeConfig";
+import { OverlayBox } from "@/components/overlay-box";
 
 const firaCode = Fira_Code({
 	subsets: ["latin"],
@@ -20,6 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${firaCode.variable} antialiased font-fira-code`}>
+				<OverlayBox></OverlayBox>
 				{children}
 			</body>
 		</html>
