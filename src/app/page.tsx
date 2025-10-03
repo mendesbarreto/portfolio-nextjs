84334b0b985053056db75086b121695845b874c6
Douglas Mendes <mendes@Douglass-Mac-Studio.local>
2025-10-02 23:09:51 -0400

    chore: Fix tailwind problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,19 @@ import { QuoteBox } from "@/components/home/quote";
 import Head from "next/head";
 
 export default function Home() {
-	return (
-		<div className="bg-amber-50 min-h-screen">
-			<div className="bg-background max-w-1064 mx-auto pt-16">
-				<Head>
-					<title>Douglas Mendes</title>
-					<meta name="description" content="My personal CV and Portifolio" />
-				</Head>
+  return (
+    <div className="bg-primary min-h-screen">
+      <div className="bg-background max-w-1064 mx-auto pt-16">
+        <Head>
+          <title>Douglas Mendes</title>
+          <meta name="description" content="My personal CV and Portifolio" />
+        </Head>
 
-				<Header />
-				<HomeSummary />
-				<QuoteBox className="flex justify-center p-8"></QuoteBox>
-				<ProjectList className="relative"></ProjectList>
-			</div>
-		</div>
-	);
+        <Header />
+        <HomeSummary />
+        <QuoteBox className="flex justify-center p-8"></QuoteBox>
+        <ProjectList className="relative"></ProjectList>
+      </div>
+    </div>
+  );
 }
