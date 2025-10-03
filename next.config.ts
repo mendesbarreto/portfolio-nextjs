9edf29266d465b883ee1f18311019cd6573e6525
Douglas Mendes <mendes@Douglass-Mac-Studio.local>
2025-10-02 23:12:14 -0400

    chore: Allow dangerouslyAllowSVG

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,8 @@ const nextConfig: NextConfig = {
 				hostname: "placehold.co",
 			},
 		],
+		dangerouslyAllowSVG: true,
+		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 	},
 	compiler: {
 		styledComponents: true,
